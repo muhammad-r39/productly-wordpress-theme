6ac25b86fa27b510e95cc15f8802ad1f0ff350ea
Muhammad Russell <muhammadrussell39@gmail.com>
2024-07-07 12:21:13 +0600

    Test

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Productly functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Productly Custom Theme
+ * @since 1.0.0
+ */
+
+/**
+ * Define Constants
+ */
+define( 'PRODUCTLY_THEME_VERSION', '1.0' );
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Productly Custom Theme
+ * @since 1.0.0
+ */
+?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <h1>Bismillah</h1>
+    <?php wp_footer(); ?>
+</body>
+</html>
